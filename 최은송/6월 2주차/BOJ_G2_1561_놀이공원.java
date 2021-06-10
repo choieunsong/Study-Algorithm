d9501b4ace87ea6a9eb93336359cc22f5489f207
choieunsong <greatgelly96@gmail.com>
2021-06-10 11:57:41 +0900

    solve: 6월 2주차 풀이

---
+++ 최은송/6월 2주차/BOJ_G2_1561_놀이공원.java
@@ -0,0 +1,72 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G2_1561_놀이공원 {
+    static int M, rides[];
+    static long N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        N = Long.parseLong(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        rides = new int[M];
+        st = new StringTokenizer(in.readLine());
+        for(int i = 0; i < M; i++)
+            rides[i] = Integer.parseInt(st.nextToken());
+
+        if(N <= M){
+            System.out.println(N);
+        }else{
+            System.out.println(binarySearch());
+        }
+    }
+    static int binarySearch(){
+        long left = 0l; long right = 60000000000l;
+        long mid = 0l;
+        while(left <= right){
+            mid = (left + right) / 2;
+            long cnt = find(mid);
+            System.out.printf("mid: %d, cnt: %d, mid+1 cnt: %d\n", mid, cnt, find(mid+1));
+
+            if(cnt <= N && N < find(mid+1)){
+                System.out.printf("gotcha! mid: %d\n", mid);
+                break;
+            } else if(cnt > N){
+                right = mid + 1;
+                System.out.printf("right+1 == left: %d, right: %d\n", left, right);
+            } else{
+                left = mid - 1;
+                System.out.printf("left-1 == left: %d, right: %d\n", left, right);
+            }
+        }
+        System.out.printf("--mid: %d, cnt: %d\n", mid, find(mid));
+        long prev = find(mid);
+        System.out.printf("prev: %d\n", prev);
+        int ret = 0;
+        for(int i = 0, idx = 0; i < M; i++){
+            if((mid % rides[i]) == 0)  idx++;
+            if(prev + idx == N){
+                ret = i;
+                break;
+            }
+        }
+        return ret;
+    }
+    static long find(long time){
+        long cnt = 0;
+        for(int i = 0; i < M; i++){
+            cnt += time / rides[i] + 1;
+        }
+        return cnt;
+    }
+}
+/*
+3 2
+7 2
+* */
---
+++ 최은송/6월 2주차/BOJ_G4_1963_소수경로.java
@@ -0,0 +1,71 @@
+package boj;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class BOJ_G4_1963_소수경로{
+    static int[] prime = new int[10000];
+    public static void main(String[] args) throws IOException {
+        BufferedReader in  = new BufferedReader(new InputStreamReader(System.in));
+        eratos();
+        int T = Integer.parseInt(in.readLine());
+        for(int t = 0; t < T; t++){
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            if(end - start == 0) {
+                System.out.println(0);
+            } else{
+                int ans = bfs(start, end);
+                System.out.printf("%s\n", ans == -1 ? "Impossible" : Integer.toString(ans));
+            }
+        }
+    }
+
+    public static void eratos(){
+        for(int i = 2; i < 10000; i++)
+            prime[i] = i;
+
+        for(int i = 2;  i < 10000; i++){
+            if(prime[i] == 0) continue;
+            for(int j = i + i; j < 10000; j += i)
+                prime[j] = 0;
+        }
+    }
+
+    public static int bfs(int start, int end){
+        int ret = -1;
+        Queue<int[]> q = new LinkedList<int[]>();
+        boolean[] used = new boolean[10000];
+        used[start] = true;
+
+        q.offer(new int[]{start, 0});
+
+        while(!q.isEmpty()) {
+            int num = q.peek()[0];
+            int cnt = q.peek()[1];
+            q.poll();
+            if (num == end) {
+                ret = cnt;
+                break;
+            }
+            StringBuilder n = new StringBuilder(Integer.toString(num));
+            for (int i = 0; i < 4; i++) {
+                int j = i == 0 ? 1 : 0;
+                StringBuilder str = new StringBuilder(n.toString());
+                for (; j < 10; j++) {
+                    str.setCharAt(i, (char) ('0' + j));
+                    num = Integer.parseInt(str.toString());
+                    if (used[num] || prime[num] == 0) continue;
+                    used[num] = true;
+                    q.offer(new int[]{num, cnt + 1});
+                }
+            }
+        }
+        return ret;
+    }
+}
